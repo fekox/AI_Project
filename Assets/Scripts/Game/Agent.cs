fbec3507fb5d91a5363b0092c348d77a2a0f8e31
fekox <facusantos123@gmail.com>
2024-09-21 13:44:40 -0300

    Update Agent.cs

--- Assets/Scripts/Game/Agent.cs
+++ Assets/Scripts/Game/Agent.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Assertions.Comparers;
 
 public enum AgentType
 {
@@ -12,7 +9,6 @@ public enum AgentType
 
 public class Agent : MonoBehaviour
 {
-
     //AgentType
     [Header("AgentType")]
     [SerializeField] private AgentType agentType;
@@ -47,17 +43,21 @@ public class Agent : MonoBehaviour
     [SerializeField] private bool isOnMine = false;
     [SerializeField] private bool isOnHome = true;
 
-    public Agent() 
-    {
-        
-    }
+    [Header("Reference: GrapfView")]
+    public GrapfView grapfView;
 
+    private List<Node<Vector2>> path = new List<Node<Vector2>>();
+    private Pathfinder<Node<Vector2>> pathfinder;
+
+    private FSM<Directions, Flags> fsm;
+
+    public Agent() { }
     public Agent(AgentType agentType,
-                 float speed, float reachDistance, bool startLoop, bool isTargetReach, 
-                 int currentFood, int maxFood, bool isFoodFull,
-                 int currentGold, int maxGold, bool isGoldFull, 
-                 float deliveringTime, float miningTime, float eatingTime,
-                 bool isOnMine, bool isOnHome)
+                 float speed, float reachDistance, bool startLoop, bool isTargetReach,  //Movement
+                 int currentFood, int maxFood, bool isFoodFull,                         //Food
+                 int currentGold, int maxGold, bool isGoldFull,                         //Gold
+                 float deliveringTime, float miningTime, float eatingTime,              //Timers
+                 bool isOnMine, bool isOnHome)                                          //Location
     {
         //AgentType
         this.agentType = agentType;
@@ -86,6 +86,49 @@ public class Agent : MonoBehaviour
         //Location
         this.isOnMine = isOnMine;
         this.isOnHome = isOnHome;
+    }
+
+    void Start()
+    {
+        InitPathfinder();
+        InitFSM();
+    }
+
+    void Update()
+    {
+        fsm.Tick();
+    }
+
+    public void InitPathfinder()
+    {
+        pathfinder = grapfView.GetPathfinderType() switch
+        {
+            PathfinderType.AStar => new AStarPathfinder<Node<Vector2>, Vector2>(),
+
+            PathfinderType.Dijkstra => new DijstraPathfinder<Node<Vector2>, Vector2>(),
+
+            PathfinderType.Breath => new BreadthPathfinder<Node<Vector2>, Vector2>(),
+
+            PathfinderType.Depth => new DepthFirstPathfinder<Node<Vector2>, Vector2>(),
+
+            _ => new AStarPathfinder<Node<Vector2>, Vector2>()
+        };
+    }
+
+    public void InitFSM()
+    {
+        fsm = new FSM<Directions, Flags>();
+
+
+
+
+        fsm.SetTransition(Directions.Wait, Flags.OnGoToTarget, Directions.WalkToMine, () => { Debug.Log("Caravan: Start"); });
+        fsm.SetTransition(Directions.WalkToMine, Flags.OnReachMine, Directions.Deliver, () => { Debug.Log("Caravan: Reach Mine"); });
+        fsm.SetTransition(Directions.Deliver, Flags.OnFoodEmpty, Directions.WalkToHome, () => { Debug.Log("Caravan: Deliver"); });
+        fsm.SetTransition(Directions.WalkToHome, Flags.OnReachHome, Directions.GatherResurces, () => { Debug.Log("Caravan: Reach Home"); });
+        fsm.SetTransition(Directions.GatherResurces, Flags.OnFoodFull, Directions.Wait, () => { Debug.Log("Caravan: Wait"); });
+
+        fsm.ForceTransition(Directions.Wait);
     }
 
 
